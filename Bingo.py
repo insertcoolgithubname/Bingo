3c8677734c77eb33d05f9b3b99d8ed9ce9e540c4
insertcoolgithubname <68465536+insertcoolgithubname@users.noreply.github.com>
2024-07-08 16:49:19 +0200

    Rewrite old functions into Bingo class

--- Bingo.py
+++ Bingo.py
@@ -21,6 +21,33 @@ def color(bool):
         return Fore.RED
 
 
+def confirm():
+    if (input("Are you sure: y/n: ")) == "y":
+        return True
+    else:
+        print("aborting")
+        return False
+
+
+def dict_index(arg_dict, key):
+    if key in dict(arg_dict).keys():
+        index = list(dict(arg_dict).keys()).index(key)
+        return index
+    else:
+        # not found
+        return (-1)
+
+
+def dict_key(arg_dict, index):
+    # returns key of index in bingo_dict
+    if index <= (len(dict(arg_dict)) - 1):
+        key = list(dict(arg_dict).keys())[index]
+        return key
+    else:
+        # not found
+        return (-1)
+
+
 class Settings():
     def __init__(self) -> None:
         self.show_completed = True
@@ -55,8 +82,109 @@ class Bingo():
     def __init__(self) -> None:
         self.main_dict = {}
         self.bingo_dict = {}
-        self.bingo_array = {}
+        for i in range(50):
+            self.main_dict[f"test{i}"] = BingoElement()
+        # self.main_dict = {"test1": BingoElement(), "test2": BingoElement()}
+        # self.bingo_dict = {"test2": BingoElement(), "test1": BingoElement()}
         self.grid_size: int = 0
+        self.bingo_array = []
+
+    def set_option_completion(self, dict: dict, index, arg_bool: bool):
+        # Completes the option of the given index from the given dict
+        try:
+            int(index)
+        except ValueError:
+            print(f"{Fore.RED}Invalid argument{Fore.RESET}")
+        else:
+            index = int(index)
+            if index <= len(dict) and index >= 0:
+                if dict[dict_key(arg_dict=dict, index=index)].complete != arg_bool:
+                    self.main_dict[dict_key(arg_dict=dict, index=index)].complete = arg_bool
+                    self.refresh()
+                    print(f"{Fore.GREEN}Sucess{Fore.RESET}")
+                else:
+                    if arg_bool:
+                        print(f"{Fore.RED}Index {index} already completed{Fore.RESET}")
+                    else:
+                        print(f"{Fore.RED}Index {index} already uncompleted{Fore.RESET}")
+            else:
+                print(f"{Fore.RED}Index out of bounds{Fore.RESET}")
+
+    def list_dict(self, dict: dict):
+        # Outputs a list of all options that are present in the grid
+        index = 0
+        completed_options = 0
+        print("")
+        for x in dict:
+            print(f"{index:02d} {x} {color((dict[x]).complete)}"
+                  f"{(dict[x]).complete}{Style.RESET_ALL}")
+            index = index + 1
+            if dict[x].complete:
+                completed_options += 1
+        print("")
+        print(f"{completed_options} out of {len(dict)} completed")
+
+    def refresh(self):
+        # Refreshes values in bingo_dict with main_dict
+        for i in self.bingo_dict:
+            self.bingo_dict[i] = self.main_dict[i]
+
+    def print_bingo(self):
+        # Outputs the bingo_arry, indexes are referencing bingo_dict"
+        grid_size = len(self.bingo_array[0])
+        print("")
+        for x in range(grid_size):
+            row = str()
+            for y in range(grid_size):
+                # if bingo_dict[list(bingo_dict.keys())[(self.bingo_array[x][y])]]:
+                if self.bingo_dict[dict_key(arg_dict=self.bingo_dict, index=self.bingo_array[x][y])].complete:
+                    row = f"{row}  {Fore.GREEN}{(self.bingo_array[x][y]):02d}{Style.RESET_ALL}"
+                else:
+                    row = f"{row}  {Fore.RED}??{Style.RESET_ALL}"
+            print(row)
+        print("")
+
+    def create_bingo_array(self, arg_size):
+        "Creates a bingo grid of the specified size, argument must be a number: create_grid 5"
+        if confirm():
+            try:
+                grid_size = int(arg_size)
+            except ValueError:
+                print(f"{Fore.RED}Invalid argument{Fore.RESET}")
+            else:
+                if (len(self.main_dict) >= grid_size * grid_size) and grid_size > 0:
+                    # create bingo array
+                    self.bingo_array = [["empty"]*grid_size for i in range(grid_size)]
+                    # create bingo_dict by choosing random indexes from main dict
+                    random_main_dict_list = random.sample(range(0, len(self.main_dict)), grid_size*grid_size)
+                    for x in range(grid_size*grid_size):
+                        self.bingo_dict[dict_key(arg_dict=self.main_dict, index=random_main_dict_list[x])] = (
+                            self.main_dict[dict_key(arg_dict=self.main_dict, index=random_main_dict_list[x])]
+                        )
+                    # fill bingo_array with randomized bingo_dict
+                    random_bingo_dict_list = random.sample(range(0, len(self.bingo_dict)), len(self.bingo_dict))
+                    index = 0
+                    for x in range(grid_size):
+                        for y in range(grid_size):
+                            self.bingo_array[x][y] = random_bingo_dict_list[index]
+                            index = index + 1
+                    print(f"{Fore.GREEN}Sucess{Fore.RESET}")
+                else:
+                    print(f"{Fore.RED}Bingo grid too large{Fore.RESET}")
+
+    def fill_main_dict(self, path):
+        # Rewrites the full list of options that can end up in your bingo grid with a file named 'input.csv', argument must specify file path: input_file C:/Users/username/path/to/your/file"
+        if confirm():
+            try:
+                with open(path+"/input.csv", "r", encoding="utf-8-sig", newline="") as f:
+                    self.main_dict = {}
+                    csvFile = csv.reader(f)
+                    for line in csvFile:
+                        self.main_dict[line[0]] = BingoElement()
+                self.refresh()
+                print(f"{Fore.GREEN}Sucess{Fore.RESET}")
+            except FileNotFoundError:
+                print(f'{Fore.RED}File not found, make sure the path to your file is correct{Fore.RESET}')
 
 
 class BingoElement():
@@ -276,6 +404,7 @@ class BingoCmd(cmd.Cmd):
 
 
 my_settings = Settings()
+my_bingo = Bingo()
 
 
 class SettingsCmd(cmd.Cmd):
@@ -324,37 +453,11 @@ def refresh():
         bingo_dict[x] = main_dict[x]
 
 
-def confirm():
-    if (input("Are you sure: y/n: ")) == "y":
-        return True
-    else:
-        print("aborting")
-        return False
-
-
-def dict_index(arg_dict, key):
-    if key in dict(arg_dict).keys():
-        index = list(dict(arg_dict).keys()).index(key)
-        return index
-    else:
-        # not found
-        return (-1)
-
-
-def dict_key(arg_dict, index):
-    # returns key of index in bingo_dict
-    if index <= (len(dict(arg_dict)) - 1):
-        key = list(dict(arg_dict).keys())[index]
-        return key
-    else:
-        # not found
-        return (-1)
-
-
 def main():
     load()
     BingoCmd().cmdloop()
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    pass
